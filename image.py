0960d86198916b495b3d4968bc2a817b3775ff68
Raghvendra Sharma <raghvendrasharma1122@gmail.com>
2024-03-12 17:46:02 +0530

    made it configurable project

--- image.py
+++ image.py
@@ -1,9 +1,15 @@
 import cv2
-src = cv2.imread("4.1 (1).jpg", cv2.IMREAD_UNCHANGED)
+
+# configurable parameters
+
+source='4.1 (1).jpg'
+destination='newAlphaImage.png'
+scale_percent = 50
+
+src = cv2.imread(source, cv2.IMREAD_UNCHANGED)
 # cv2.imshow("Image", src)
 
 #percent.by which the image is.resized 
-scale_percent = 50
 
 #calculate the 50 percent of original dimensions 
 width = int(src.shape[1] + scale_percent / 100) 
@@ -15,5 +21,5 @@ dsize = (width, height)
 # resize image 
 output = cv2.resize(src, dsize)
 
-cv2.imwrite('newAlphaImage.png',output) 
+cv2.imwrite(destination,output) 
 cv2.waitKey(0)
